8940752fb171b47d64d578b71d99346f5ae826d1
ShelbyThromas <aamto48@gmail.com>
2023-06-07 14:44:11 +0700

    Add project files.

---
+++ ConsoleApp_First/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp_First
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int y = 0;
+            while(y <3)
+            }
+        }
+    }
+}
+//console.writeline("hello, world!");
