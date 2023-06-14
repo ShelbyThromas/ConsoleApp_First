bc658cfc5c05fdf9a82424ae170693a340dbc560
ShelbyThromas <aamto48@gmail.com>
2023-06-14 11:39:08 +0700

    เพิ่มมาใหม่

--- ConsoleApp_First/Program.cs
+++ ConsoleApp_First/Program.cs
@@ -6,15 +6,79 @@ namespace ConsoleApp_First
     {
         static void Main(string[] args)
         {
+            #region
             int y = 0;
-            while(y <3)
+            while (y < 3)
             {
                 Console.WriteLine("Shelby Thromas " + y);
                 y++;
             }
             Console.WriteLine("Loop ended"); //loopend
+        
+        #endregion
+
+        #region
+        string[] BrandTV = { "LG", "SONY", "SAMSUNG", "TCL", "JVC" };
+        foreach (string television in BrandTV)
+            {
+            Console.WriteLine("Brand_Shelby" + television);
             }
-        }
-    }
+    #endregion
+}
+}
+}
 
 //console.writeline("hello, world!");
+/* if (20>18)
+ * {
+ * Console.Writeline("20 is Greater than 18")
+ * }
+ */
+
+/*if else
+ * int time = 20 ;
+ *if (time < 18)
+ *{
+ *Console.Writeline("Good day.");
+ *}
+ *else
+ *{
+ *Console.Writeline("Good Evening.");
+ *}
+ */
+
+/*if else if else
+ * int time = 22;
+ * if (time < 10){
+ * Console.WriteLine("Good bad");
+ * }else if (time <20) {
+ * Console.Writeline("Good Bye.");
+ * } else {
+ * Console.WriteLine("Good evening.");
+ * }
+ */
+
+/*switch(exprssion)
+ * {
+ * case result_expression1 : // block of code to be executed.break;
+ * case result_expression2 : // block of code to be executed.break;
+ * defualt : // block of code to be executed.break;
+ * }
+ */
+
+/*for (int iterator = 0; iterator < 5: iterator++)
+ * {
+ * Console.Writeline("Counter"+i0;
+ * }
+ */
+
+/*for (int iterator =0; iterator < 3;iterator++)
+ * {
+ * Console.WriteLine("Outer Loop :"+iterator);
+ * 
+ * for(int_iterator = 0;_iterator < 20; ++_iterator)
+ * {
+ * Console.WriteLine("Inner Loop:"+i); // 2 time
+ * }
+ * }
+ */
